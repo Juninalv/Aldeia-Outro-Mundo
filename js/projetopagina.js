98004bb66ce91cbc37be51d35696ebb37ad75120
Juninho Alves <junioralv290302@gmail.com>
2026-06-28 21:27:28 -0300

    ajustes mobile

--- js/projetopagina.js
+++ js/projetopagina.js
@@ -185,3 +185,24 @@ projectToggles.forEach((button) => {
     }
   });
 });
+/* MENU MOBILE */
+
+const menuToggle = document.querySelector("#menu-toggle");
+const nav = document.querySelector("#nav");
+
+if (menuToggle && nav) {
+  menuToggle.addEventListener("click", () => {
+    nav.classList.toggle("active");
+
+    menuToggle.classList.toggle("open");
+  });
+
+  // fecha ao clicar em algum link
+
+  nav.querySelectorAll("a").forEach((link) => {
+    link.addEventListener("click", () => {
+      nav.classList.remove("active");
+      menuToggle.classList.remove("open");
+    });
+  });
+}
